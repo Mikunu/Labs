a0518b5cc84b2254d8e8ac530a90ce9fe4962c87
Mikunu <58949015+Mikunu@users.noreply.github.com>
2021-05-20 06:09:53 +0600

    Add files via upload

---
+++ saitovlaboop1/saitovlaboop1/saitovlaboop1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <windows.h>
+
+class Vector
+{
+private:
+	double x, y, z;
+
+public:
+	void set_vector(double x, double y, double z) {
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+
+	double get_square()
+	{
+		return x * x + y * y + z * z;
+	}
+};
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	double x, y, z;
+	std::cout << "Введите x: ";
+	std::cin >> x;
+	std::cout << "Введите y: ";
+	std::cin >> y;
+	std::cout << "Введите z: ";
+	std::cin >> z;
+
+	Vector vec3;
+	vec3.set_vector(x, y, z);
+	std::cout << "Квадрат вектора: " << vec3.get_square();
+
+	return 0;
+}
---
+++ saitovlaboop2/saitovlaboop2/saitovlaboop2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <windows.h>
+
+class Stack
+{
+private:
+    int val;
+public:
+    Stack(int n);
+    ~Stack();
+};
+
+Stack::Stack(int n)
+{
+    val = n;
+    std::cout << val;
+}
+
+Stack::~Stack()
+{
+    std::cout << "Память освобождена";
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Stack* stack;
+    stack = new Stack(5);
+    std::cout << '\n';
+    delete stack;
+}
